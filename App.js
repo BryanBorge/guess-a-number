fe29a634f96abebdec9c720761f81ea46a7323ee
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-24 12:51:38 -0400

    Added conditional rendering of different screensd

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNum, setUserNum] = useState();
+
+  const startGameHandler = selectedNum => {
+    setUserNum(selectedNum);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNum) {
+    content = <GameScreen userChoice={userNum} />;
+  }
   return (
     <View style={styles.screen}>
       <Header title='Guess a Number' />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
