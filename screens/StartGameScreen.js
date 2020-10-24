2aeb2bdc8b3f450a2a7db5ba92bf93b6f22146ba
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-24 11:27:52 -0400

    Added Card component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { View, Text, TextInput, StyleSheet, Button } from "react-native";
-
+import Card from "../components/Card";
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a new game</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title='Reset' />
           <Button title='Confirm' />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -23,28 +23,16 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: "center",
   },
-  title: {
-    fontSize: 20,
-    marginVertical: 10,
-  },
   inputContainer: {
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    // These shadows properties only work on ios
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    //Android uses elevation
-    elevation: 5,
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
   },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+
   buttonContainer: {
     flexDirection: "row",
     width: "100%",
